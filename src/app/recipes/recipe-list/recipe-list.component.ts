f31bd219279ed866b1ee2c1cafc0b9b9f558a8cb
Haydar <haydaramrurevanda@mail.ugm.ac.id>
2023-08-08 23:35:00 +0700

    added recipe-list and recipe-item

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe("A Test Recipe", "This is simply a test", "https://img.taste.com.au/mOx3fOxf/w720-h480-cfill-q80/taste/2022/09/garlic-chilli-prawn-pasta-181440-1.jpg"),
+    new Recipe("A Test Recipe", "This is simply a test", "https://img.taste.com.au/mOx3fOxf/w720-h480-cfill-q80/taste/2022/09/garlic-chilli-prawn-pasta-181440-1.jpg")
+  ];
+
+  constructor() {}
+
+  ngOnInit() {}
+}
